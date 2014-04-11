7f8f1a6a775ed2d5f772dc1c54f01b06a97e9be9
obemgcabazn <obemgghost@ya.ru>
2014-04-11 21:05:41 +0300

    vangsang/vansan

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Landing Page</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container-full">
+	<div class="row">
+		<div class="col-lg-6">123</div>
+		<div class="col-lg-6">123</div>
+	</div>
+</div>
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
